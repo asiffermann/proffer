bfe71d84407fd9c435e327395e0cb89a5ec424eb
Adrien Siffermann <adrien@siffermann.me>
2021-05-01 04:17:04 +0200

    Add Proffer.Email.Integration.Test project #27

---
+++ tests/Proffer.Email.Integration.Test/IntegrationCollection.cs
@@ -0,0 +1,9 @@
+namespace Proffer.Email.Integration.Test
+{
+    using Xunit;
+
+    [CollectionDefinition(nameof(IntegrationCollection))]
+    public class IntegrationCollection: ICollectionFixture<StoresFixture>
+    {
+    }
+}
---
+++ tests/Proffer.Email.Integration.Test/Utils.cs
@@ -0,0 +1,10 @@
+﻿namespace Proffer.Email.Integration.Test
+{
+    public static class Utils
+    {
+        public static T[] Yield<T>(this T item)
+        {
+            return new T[1] { item };
+        }
+    }
+}
